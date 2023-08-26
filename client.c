79be65118f5b28ab4a9be2beba65c28c11c9899c
Lymh <lymhmorales@gmail.com>
2023-08-26 12:02:25 -0600

    Combination of 2 messages

--- client.c
+++ client.c
@@ -2,19 +2,7 @@
 #include<stdlib.h>
 //holaaaaaaaaaa
 //
-//cambiando un par de cosa
-//holaaaaaaaaaa
-//
-//cambiando un par de cosas
-//holaaaaaaaaaa
-//
-//cambiando un par de cosas
 
-#include<stdbool.h>//holaaaaaaaaaa
-//
-//cambiando un par de cosas
-
-//Here from the first commit
 ayudaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 corriendo
 
