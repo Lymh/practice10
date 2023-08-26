df3a33de6e1d43a52d6d7e831d9d2b8a8053ed67
Lymh <lymhmorales@gmail.com>
2023-08-26 12:02:25 -0600

    adding new message

--- client.c
+++ client.c
@@ -2,4 +2,18 @@
 #include<stdlib.h>
 //holaaaaaaaaaa
 //
+//cambiando un par de cosa
+//holaaaaaaaaaa
+//
 //cambiando un par de cosas
+//holaaaaaaaaaa
+//
+//cambiando un par de cosas
+
+#include<stdbool.h>//holaaaaaaaaaa
+//
+//cambiando un par de cosas
+
+//Here from the first commit
+
+
