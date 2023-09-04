97f59a098a61237e349fec547a64a56c36b7ea2f
Lymh <lymhmorales@gmail.com>
2023-09-03 19:42:42 -0600

    Practicing

--- client.c
+++ client.c
@@ -2,9 +2,9 @@
 #include<stdlib.h>
 //holaaaaaaaaaa
 //
-
 ayudaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 corriendo
 taking just hi
 combinacion agregando un nuevo comentario de la combinacion
 arreglando comentario para el primer commit
+pensando en una solucion
